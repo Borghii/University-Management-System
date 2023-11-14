fcae97b4d20d3ea01c243ce6ef1274863908c4ea
Borghii <137845283+Borghii@users.noreply.github.com>
2023-11-13 23:05:44 -0300

    feat: add Stage of JavaFX

--- src/main/java/com/example/universitymanagementsystem/HelloController.java
+++ src/main/java/com/example/universitymanagementsystem/HelloController.java
@@ -2,13 +2,22 @@ package com.example.universitymanagementsystem;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+    @FXML
+    private TextField nombre;
+    @FXML
+    private TextField apellido;
+
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        welcomeText.setText("Su nombre es: "+nombre.getText()+" " +apellido.getText());
     }
+
+
+
 }
